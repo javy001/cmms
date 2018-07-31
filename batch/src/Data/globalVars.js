8dc150994882f8e68fce95db403aaa45c404b8c9
Javier Quintero <javierq@fb.com>
2018-07-30 17:54:51 -0700

    fixed url bug

--- batch/src/Data/globalVars.js
+++ batch/src/Data/globalVars.js
@@ -1,3 +1,3 @@
 export const uriSubDir = '/test';
-// export const url = 'http://ec2-34-217-104-207.us-west-2.compute.amazonaws.com/api';
-export const url = 'http://localhost:5000';
+export const url = 'http://ec2-34-217-104-207.us-west-2.compute.amazonaws.com/api';
+// export const url = 'http://localhost:5000';
